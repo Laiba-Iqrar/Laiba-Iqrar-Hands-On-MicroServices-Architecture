64be234b76e58f5674f8276578f1bc32e4c192f0
Laiba-Iqrar <laibaiqrarahmedkhan@gmail.com>
2024-12-29 21:35:02 +0500

    added comments

--- user-service/src/routes/authRoutes.ts
+++ user-service/src/routes/authRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import AuthController from "../controllers/AuthController";
 
 const userRouter = Router();
-
+//USED ROUTERSS
 userRouter.post("/register" as any, AuthController.register);
 userRouter.post("/login" as any, AuthController.login);
 
